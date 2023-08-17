9a3863b07a74a9c64bb336a4a59b819aa3728db8
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-08-18 01:18:45 +0300

    async / await veri çekme

--- script.js
+++ script.js
@@ -145,10 +145,30 @@ const priceList = () => {
 
 //! fetch ile veri çekme
 
-function getUserData() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+// function getUserData() {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
+
+// getUserData();
+
+// //?---------------------------
+// //?---------------------------
+// //?---------------------------
+
+//! async / await veri çekme
+
+async function getUserData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('işlem tamamlandı');
+  }
 }
 
 getUserData();
