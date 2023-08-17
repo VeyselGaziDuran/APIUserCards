02ee756f72620dc633f9bd072226c4bd896852d1
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-08-18 01:43:27 +0300

    HTML / Css

--- script.js
+++ script.js
@@ -162,13 +162,37 @@ const priceList = () => {
 async function getUserData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    console.log(data);
+    const userList = await response.json();
+    getUserList(userList);
   } catch (error) {
     console.log(error);
   } finally {
     console.log('işlem tamamlandı');
   }
+}
+
+function getUserList(userList) {
+  const userListDOM = document.querySelector('.user-list');
+  let userItem = '';
+  userList.forEach((user) => {
+    userItem += `
+    <li class="user-card">
+      <img
+        src="https://i.pravatar.cc/150?img=${user.id}"
+        alt="${user.name}"
+        class="user-image"
+      />
+      <div class="user-info">
+        <h3 class="user-name">${user.name}</h3>
+        <p class="user-username">${user.username}</p>
+        <p class="user-email">${user.email}</p>
+        <p class="user-website">${user.website}</p>
+        <p class="user-userId">${user.id}</p>
+      </div>
+    </li>
+    `;
+  });
+  userListDOM.innerHTML = userItem;
 }
 
 getUserData();
